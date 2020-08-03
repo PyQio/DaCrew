3c91003361f97d8818f1b3195fdf9f2e5c46b5ee
PyQio <sikopugliese@gmail.com>
2020-08-03 18:21:35 +0200

    command?

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ client.on('message', message => {
     const args = message.content.slice(PREFIX.length).split(/ +/)
     const command = args.shift().toLowerCase()
 
-    if (!client.commands.has(command)) return;
+    if (!client.command.has(command)) return;
 
     try {
-        client.commands.get(command).execute(message, args);
+        client.command.get(command).execute(message, args);
     } catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
