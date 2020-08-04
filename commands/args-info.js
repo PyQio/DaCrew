08ebcc7e3c7cf165001161042c55f21a7b8173df
PyQio <sikopugliese@gmail.com>
2020-08-04 10:21:23 +0200

    Comments
    Added some comments, especially in index.js. I then changed client.command to client.commands, considering it's a collection of different commands

--- commands/args-info.js
+++ commands/args-info.js
@@ -1,12 +1,14 @@
 module.exports = {
 	name: 'args-info',
-    description: 'Information about the arguments provided.',
+	description: 'Information about the arguments provided.',
+	// this is just a test for any kind of command with any number of args,
+	// so args is needed to be set true. See ../index.js for more details
     args: true,
 	execute(message, args) {
 		if (args[0] === 'foo') {
 			return message.channel.send('bar')
 		}
-
+		
 		message.channel.send(`Arguments: ${args}\nArguments length: ${args.length}`)
 	},
 };
--- index.js
+++ index.js
@@ -1,36 +1,52 @@
+// random prefix chosen as the first char. Each command will have this 
+// format: PREFIX + ${command}. You can easily change the PREFIX just by
+// changing this const.
 const PREFIX = '+'
 
 const fs = require ('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.command = new Discord.Collection()
+// this first section of the code reads the file used in ./commands/${file}
+// for each command (command: PREFIX + ${command})
+client.commands = new Discord.Collection()
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
 for(const file of commandFiles) {
     const command = require(`./commands/${file}`)
 
-    client.command.set(command.name, command)
+    client.commands.set(command.name, command)
 }
 
+// once the bot is ready, you should read Logged in as ${nameChosenForTheBot}!
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('message', message => {
+    // if the message doesn't start with the prefix or the author is the bot, then 
+    // the message isn't considered.
     if(!message.content.startsWith(PREFIX) || message.author.bot) return
 
+    // if the message could be a command, it will split the args based on / +/,
+    // aka a space (' ')
     const args = message.content.slice(PREFIX.length).split(/ +/)
     const commandName = args.shift().toLowerCase()
 
-    if (!client.command.has(commandName)) return
+    // once command and args are split, we can now check whether or not
+    // the command exists in the client.commands Collection
+    if (!client.commands.has(commandName)) return
 
-    const command = client.command.get(commandName);
+    // the command itself
+    const command = client.commands.get(commandName);
     
+    // if args are important or needed for the command, please remember to set args: true 
+    // in the module.exports, otherwise this check won't be considered
     if (command.args && !args.length) 
         return message.channel.send(`You didn't provide any arguments, ${message.author}!`)
 
     try {
-        client.command.get(commandName).execute(message, args)
+        // the proper execution of the command
+        client.commands.get(commandName).execute(message, args)
     } catch (error) {
         console.error(error)
         message.reply('there was an error trying to execute that command!')
